d93bb539eb0140dedcfd5c5b6f160ed1b3d1ba58
sialou <floraskouassi@hotmail.fr>
2018-04-09 10:41:08 +0000

    Update listestudent.php

--- listestudent.php
+++ listestudent.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -271,17 +271,17 @@
 
   <!-- RS5.0 Core JS Files -->
   
-  <script type="text/javascript" src="js/jquery.themepunch.tools.min.js?rev=5.0"></script>
-  <script type="text/javascript" src="js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
-  <script type="text/javascript" src="js/revolution.extension.actions.min.js"></script>
-  <script type="text/javascript" src="js/revolution.extension.carousel.min.js"></script>
-  <script type="text/javascript" src="js/revolution.extension.kenburn.min.js"></script>
-  <script type="text/javascript" src="js/revolution.extension.layeranimation.min.js"></script>
-  <script type="text/javascript" src="js/revolution.extension.migration.min.js"></script>
-  <script type="text/javascript" src="js/revolution.extension.navigation.min.js"></script>
-  <script type="text/javascript" src="js/revolution.extension.parallax.min.js"></script>
-  <script type="text/javascript" src="js/revolution.extension.slideanims.min.js"></script>
-  <script type="text/javascript" src="js/revolution.extension.video.min.js"></script>
+  <script  src="js/jquery.themepunch.tools.min.js?rev=5.0"></script>
+  <script  src="js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
+  <script  src="js/revolution.extension.actions.min.js"></script>
+  <script  src="js/revolution.extension.carousel.min.js"></script>
+  <script  src="js/revolution.extension.kenburn.min.js"></script>
+  <script  src="js/revolution.extension.layeranimation.min.js"></script>
+  <script  src="js/revolution.extension.migration.min.js"></script>
+  <script  src="js/revolution.extension.navigation.min.js"></script>
+  <script  src="js/revolution.extension.parallax.min.js"></script>
+  <script  src="js/revolution.extension.slideanims.min.js"></script>
+  <script  src="js/revolution.extension.video.min.js"></script>
 
   <!-- my script--> 
   <script src="js/main.js"></script>
